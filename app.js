67539d26d6737015d79d4c29f90031088e29d7a1
mohamed-jebali <mohamedjebali992010@gmail.com>
2025-02-02 17:02:24 +0100

    chess setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const chessPieces = document.querySelectorAll('.chess-piece');
+
+const square = document.querySelectorAll('.square');
+
+const info = document.getElementById('info');
+
+
+console.log(knight , square);
